c78c1e56908d7fff59524f585ce7aaf58b3b205d
HenriqueOPeixoto <henrique.peixoto1512@gmail.com>
2021-12-28 15:51:06 -0400

    docs: info de autoria do código

---
+++ Main.c
@@ -0,0 +1,16 @@
+/**
+ * @file Main.c
+ * @author Henrique Peixoto (henrique.peixoto1512@gmail.com)
+ * @brief Resolução do Problema B.
+ * @version 0.1
+ * @date 2021-12-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
